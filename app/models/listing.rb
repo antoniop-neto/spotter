366398fc099dd04e7ce9f06326ed64323406b385
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-03-12 14:51:45 +0000

    validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,5 @@
 class Listing < ApplicationRecord
   belongs_to :user
+
+  validates :name, :location, :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   has_many :listings
+  has_many :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, presence: true
 end
