144c7617d55aa05b450c192eba0a1446b9b37976
Ningjie Cao <kromatiks1@gmail.com>
2024-03-13 00:44:32 +0800

    changed views for bookings and listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 peter = User.create!(email: 'peter@gmail.com', password: 'password123', password_confirmation: 'password123', first_name: 'Peter', last_name: 'Doe')
 john = User.create!(email: 'john@yahoo.com', password: 'password123', password_confirmation: 'password123', first_name: 'John', last_name: 'Crawford')
 jane = User.create!(email: 'jane@yahoo.com', password: 'password123', password_confirmation: 'password123', first_name: 'Jane', last_name: 'Austen')
+emily = User.create!(email: 'emily@yahoo.com', password: 'password123', password_confirmation: 'password123', first_name: 'Emily', last_name: 'Bronte')
 
 # Create listings
 mountain = Listing.create!(name: "Cozy Mountain Retreat", location: "Aspen, Colorado", wifi: true, workspace: true, price: 150.50, availability: true, user: peter)
@@ -13,3 +14,4 @@ lakehouse = Listing.create!(name: "Lake House Getaway", location: "Lake Tahoe, C
 # Create bookings
 Booking.create!(start_date: Date.new(2023, 3, 15), end_date: Date.new(2023, 3, 20), listing: mountain, user: jane)
 Booking.create!(start_date: Date.new(2023, 4, 10), end_date: Date.new(2023, 4, 15), listing: cottage, user: jane)
+Booking.create!(start_date: Date.new(2023, 5, 12), end_date: Date.new(2023, 5, 17), listing: bungalow, user: emily)
