293e3790895855d8e21ed93ea82ebf0ed5d578b0
Ningjie Cao <kromatiks1@gmail.com>
2024-03-12 19:41:36 +0800

    view-bookings basic setup completed

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # routes for Bookings
+  get "bookings", to: "bookings#index", as: :bookings
 end
