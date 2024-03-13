b083d50d1da2b06cdf0b1cd6bd4075c9c62c89ea
Ningjie Cao <kromatiks1@gmail.com>
2024-03-13 20:02:44 +0800

    destroy methods and manage action

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,22 @@
 class ListingsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @listings = Listing.all
   end
 
   def show
     @listing = Listing.find(params[:id])
+  end
+
+  def manage
+    @user = current_user
+    @listings = current_user.listings
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    redirect_to listings_path, status: :see_other
   end
 end
