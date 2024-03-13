2e2a35351c5097819791703f12b1cda73ddaa0f6
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-03-13 13:22:01 +0000

    adding new and create action to Listings Controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,4 +6,25 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find(params[:id])
   end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.user = current_user
+    if @listing.save
+      redirect_to listings_path(@listings)
+    else
+      render :new, status: :unprocessable_entity
+      # this is when you fail to book, you will come back to new booking page
+    end
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:name, :location, :price, :photo, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'listings', to: 'listings#index'
+  get 'listings/new', to: 'listings#new', as: :new
+  post 'listings', to: 'listings#create'
   get 'listings/:id', to: 'listings#show', as: :listing
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
