e2be41f9771b09b938adf00c946e6c12791567c6
Ningjie Cao <kromatiks1@gmail.com>
2024-03-15 01:00:13 +0800

    homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @listings = Listing.order(created_at: :desc).limit(4)
   end
 end
