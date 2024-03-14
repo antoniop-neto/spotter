569f473eb34cf821f73666e5242ddfb3671f4b18
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-03-14 12:05:47 +0000

    Add the map in the index and show page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,17 +3,31 @@ class ListingsController < ApplicationController
 
   def index
     @listings = Listing.all
+    @markers = @listings.geocoded.map do |listing|
+      {
+        lat: listing.latitude,
+        lng: listing.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { listing: listing })
+      }
+    end
   end
 
   def show
     @listing = Listing.find(params[:id])
+    @markers = [
+      {
+        lat: @listing.latitude,
+        lng: @listing.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { listing: @listing })
+      }
+    ]
   end
 
   def manage
     @user = current_user
     @listings = current_user.listings
   end
-  
+
   def new
     @listing = Listing.new
   end
@@ -21,12 +35,21 @@ class ListingsController < ApplicationController
   def create
     @listing = Listing.new(listing_params)
     @listing.user = current_user
-    if @listing.save
-      redirect_to listings_path(@listings)
-    else
-      render :new, status: :unprocessable_entity
-      # this is when you fail to book, you will come back to new booking page
+    respond_to do |form|
+      if @listing.save
+        form.html { redirect_to listing_url(@listing), notice: "Listing was successfully created." }
+        form.json { render :show, status: :created, location: @listing }
+      else
+        form.html { render :new, status: :unprocessable_entity }
+        form.json { render json: @listing.errors, status: :unprocessable_entity }
+      end
     end
+    # if @listing.save
+    #   redirect_to listings_path(@listings)
+    # else
+    #   render :new, status: :unprocessable_entity
+    #   # this is when you fail to book, you will come back to new booking page
+    # end
   end
 
   def destroy
@@ -34,11 +57,10 @@ class ListingsController < ApplicationController
     @listing.destroy
     redirect_to listings_path, status: :see_other
   end
-  
+
   private
 
   def listing_params
     params.require(:listing).permit(:name, :location, :price, :photo, :user_id)
   end
-  
 end
